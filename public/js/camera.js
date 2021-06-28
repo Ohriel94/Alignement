b7a8caafa7c3c5a5cdd1befa18d8eca9916023e8
Ohriel94 <brendb@outlook.com>
2021-06-28 13:20:46 -0400

    Logo fournier

--- public/js/camera.js
+++ public/js/camera.js
@@ -1,34 +1,44 @@
-var c = document.getElementById("canvas");
-var ctx = c.getContext("2d");
-var rotation = 90;
+const linesThickness = 1;
+const alpha = 1;
+const linesColour = "rgba(255,176,72,"+alpha+")";
+const x = 30;
+const y = 30;
+const step = 30;
+const height = 720;
+const width = 1080;
 
-const linesThickness = 10;
-const alpha = 0.95;
+var page = document.getElementById("canvas-couche-1");
+var ctx = page.getContext("2d");
 
-ctx.fillRect(0,0,500,500);
-ctx.fillStyle = "#FFFFFF";
+ctx.fillStyle = "rgba(255,255,255,0.10)";
+ctx.fillRect(0,0,width,height);
 
-// ctx.beginPath();
-// ctx.lineCap = "round";
-// ctx.strokeStyle = "rgba(255,176,72,"+alpha+")";
-// ctx.moveTo(0, 50);
-// ctx.lineTo(1500, 50);
-// ctx.lineWidth = linesThickness;
-// ctx.stroke();
+for (let i = 0 ; i < width / 31 ; i++) {
+    ctx.beginPath();
+    ctx.lineCap = "round";
+    ctx.strokeStyle = linesColour;
+    ctx.moveTo(x + step * i, 0);
+    ctx.lineTo(y + step * i, height);
+    ctx.lineWidth = linesThickness;
+    ctx.stroke();
+}
 
-// ctx.beginPath();
-// ctx.lineCap = "round";
-// ctx.strokeStyle = "rgba(255,176,72,"+alpha+")";
-// ctx.moveTo(0, 20);
-// ctx.lineTo(1500, 20);
-// ctx.lineWidth = linesThickness;
-// ctx.stroke();
-// ctx.globalCompositeOperation = "destination-atop";
+for (let i = 0 ; i < height / 32 ; i++) {
+    ctx.beginPath();
+    ctx.lineCap = "round";
+    ctx.strokeStyle = linesColour;
+    ctx.moveTo(0, y + step * i);
+    ctx.lineTo(width, y + step * i);
+    ctx.lineWidth = linesThickness;
+    ctx.stroke();
+}
 
-
+ctx.globalCompositeOperation = "destination-atop";
 
 function Coulisser() {
-
+    ctx.clearRect(0, 0, 1080, 720);
+    ctx.translate(100,100)
+    ctx.stroke();
 }
 
 function Orienter() {
