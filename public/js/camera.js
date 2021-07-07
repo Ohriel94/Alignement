e3a7db4c0b3d4abe3ecbfdc57519118363f61fa4
Ohriel94 <brendb@outlook.com>
2021-07-07 08:33:07 -0400

    page05 devient page01

--- public/js/camera.js
+++ public/js/camera.js
@@ -1,46 +1,60 @@
-const linesThickness = 1;
-const alpha = 1;
-const linesColour = "rgba(255,176,72,"+alpha+")";
-const x = 30;
-const y = 30;
-const step = 30;
+const x = 0;
+const y = 0;
 const height = 720;
 const width = 1080;
+const linesColour = "rgba(255,176,72," + alpha.value / 10 + ")";
 
-var page = document.getElementById("canvas-couche-1");
-var ctx = page.getContext("2d");
-
-ctx.fillStyle = "rgba(255,255,255,0.10)";
-ctx.fillRect(0,0,width,height);
-
-for (let i = 0 ; i < width / 31 ; i++) {
-    ctx.beginPath();
-    ctx.lineCap = "round";
-    ctx.strokeStyle = linesColour;
-    ctx.moveTo(x + step * i, 0);
-    ctx.lineTo(y + step * i, height);
-    ctx.lineWidth = linesThickness;
-    ctx.stroke();
-}
-
-for (let i = 0 ; i < height / 32 ; i++) {
-    ctx.beginPath();
-    ctx.lineCap = "round";
-    ctx.strokeStyle = linesColour;
-    ctx.moveTo(0, y + step * i);
-    ctx.lineTo(width, y + step * i);
-    ctx.lineWidth = linesThickness;
-    ctx.stroke();
-}
+var thickness = document.getElementById("thickness");
+var alpha = document.getElementById("alpha");
+var step = document.getElementById("step");
+var valueThick = document.getElementById("valueThick");
+var valueAlpha = document.getElementById("valueAlpha");
+var valueStep = document.getElementById("valueStep");
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
 
 ctx.globalCompositeOperation = "destination-atop";
+ctx.fillStyle = "rgba(255,255,255,0.10)";
+ctx.fillRect(0, 0, width, height);
 
-function Coulisser() {
-    ctx.clearRect(0, 0, 1080, 720);
-    ctx.translate(100,100)
-    ctx.stroke();
+thickness.oninput = function() {
+    valueThick.innerHTML = this.value
+    drawGrid(width,height,linesColour,thickness,alpha,step);
+}
+alpha.oninput = function() {
+    valueAlpha.innerHTML = this.value / 10;
+    drawGrid(width,height,linesColour,thickness,alpha,step);
+}
+step.oninput = function() {
+    valueStep.innerHTML = this.value
+    drawGrid(width,height,linesColour,thickness,alpha,step);
 }
 
-function Orienter() {
+function drawGrid(width,height,linesColour,thickness,alpha,step) {
+    for (let i = 0; i < width / 31; i++) {
+        ctx.beginPath();
+        ctx.lineCap = "round";
+        ctx.strokeStyle = linesColour;
+        ctx.moveTo(x + step * i, 0);
+        ctx.lineTo(y + step * i, height);
+        ctx.lineWidth = thickness.value;
+        ctx.stroke();
+    }
     
+    for (let i = 0; i < height / 32; i++) {
+        ctx.beginPath();
+        ctx.lineCap = "round";
+        ctx.strokeStyle = linesColour;
+        ctx.moveTo(0, y + step.value * i);
+        ctx.lineTo(width, y + step.value * i);
+        ctx.lineWidth = thickness.value;
+        ctx.stroke();
+    }
+}
+
+function clear() {
+    var canvasTemp = canvas;
+    var ctxTemp = canvasTemp.getContext("2d");
+    ctxTemp.clearRect(0, 0, width, height);
+
 }
