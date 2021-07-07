ded0eb4a92857ff2c655caeaecf62de8cf37c12a
Ohriel94 <brendb@outlook.com>
2021-07-07 10:04:12 -0400

    page05 devient page01

--- src/Controller/index.php
+++ src/Controller/index.php
@@ -12,5 +12,38 @@ use Symfony\Component\HttpFoundation\Cookie;
 
             ]);
         }
+
+        public function aff_p2(): Response
+        {
+            return $this->render('pages/page02.html.twig', [
+
+            ]);
+        }
+
+        public function aff_p3(): Response
+        {
+            return $this->render('pages/page03.html.twig', [
+
+            ]);
+        }
+
+        public function aff_p4(): Response
+        {
+            return $this->render('pages/page04.html.twig', [
+
+            ]);
+        }
+
+        public function aff_p5(): Response
+        {
+            $response = new Response(
+                'Content',
+                Response::HTTP_OK,
+                ['content-type' => 'text/html']);
+            $response->headers->setCookie(Cookie::create('foo', 'bar'));
+            return $this->render('pages/page05.html.twig', [
+
+            ]);
+        }
         
     }
